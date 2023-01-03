52aa60f3d72b1f4212cb9082127c0010fbbb357e
Aaron David <ajdavid128@gmail.com>
2023-01-03 11:28:11 -0600

    uninstalled react router v6 and semantic ui and reinstalled using the --prefix client in the command, this fixed the error that prevented the site form loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {useState, useEffect} from 'react'
 import Nav from './Components/Nav';
